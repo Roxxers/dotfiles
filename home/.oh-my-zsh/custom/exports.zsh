5387d04584714558ef4deeff7c0a2d91105fb9f7
Roxie Gibson <me@roxxers.xyz>
2019-10-23 00:44:12 +0100

    add pipenv config to exports

--- home/.oh-my-zsh/custom/exports.zsh
+++ home/.oh-my-zsh/custom/exports.zsh
@@ -3,6 +3,8 @@ export SHELL="/usr/bin/zsh";
 
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
 export PYTHONIOENCODING='UTF-8';
+# Pipenv config for making venvs in project dir
+export PIPENV_VENV_IN_PROJECT=1
 
 # Prefer UK English and use UTF-8.
 export LANG='en_GB.UTF-8';
