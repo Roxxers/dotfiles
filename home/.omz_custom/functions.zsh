8b3aabfe976f8d3b4a45f9d63cc97d30688d2773
Roxie Gibson <me@roxxers.xyz>
2020-03-12 04:42:24 +0000

    added tomb wrapper function to remove and enable swap

--- home/.omz_custom/functions.zsh
+++ home/.omz_custom/functions.zsh
@@ -1,4 +1,12 @@
 # Create a new directory and enter it
-function mkd()
+function mkd() {
 	mkdir -p "$argv";cd "$argv";
+}
+
+function tombs() {
+	sudo swapoff -a
+	tomb $@
+	sudo swapon /swapfile
+	swapon --show
+}
 
